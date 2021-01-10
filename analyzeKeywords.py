f0027de720f937f51cc18b4daf90d0aaaa58a948
semih <semihsolmaz@hotmail.com>
2021-01-10 20:34:59 +0300

    changed file names

--- analyzeKeywords.py
+++ analyzeKeywords.py
@@ -1,3 +1,97 @@
+import pandas as pd
+import numpy as np
+import re
+import tweepy
+import nltk
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+from collections import Counter
+
+
+
+def contain_related_word(text):
+    look_up_words = ['bug', 'buggy', 'freeze', 'slow', 'crash']
+    tokens = text.split()
+    t = [x in tokens for x in look_up_words]
+    if True in t:
+        return True
+    else:
+        return False
+
+
+def clean_tweet(tweet):
+    '''
+    Utility function to clean tweet text by removing links, special characters
+    using simple regex statements.
+    '''
+    return ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)", " ", tweet).split())
+
+
+auth = tweepy.OAuthHandler('dT8QrbTNl2Iv5RZ2vqqsSB4Ra', 'tpUB3N5MkrQ7tUwYSWpyQI90ytuz8WHyJVoCbZ5o2FlpUFRtEk')
+auth.set_access_token('2483903256-UHDqrWSYTP28TOc5kzQNL3RH3xXJHTiUG3Y4sg1', 'tPSEJnQOwBwr4H1wK2UPZRqMWUAHANqaUrJLKYNjbemjI')
+
+api = tweepy.API(auth)
+
+fetched_tweets = api.search(q='Qatar airways', count=20)
+def get_most_used_words(fetched_tweets, word_type):
+    tweet_words_list = []
+    for tweet in fetched_tweets:
+        tokens = nltk.word_tokenize(clean_tweet(tweet.text))
+        text = nltk.Text(tokens)
+        tags = nltk.pos_tag(text)
+        filtered = filter(lambda tag: tag[1] == word_type, tags)
+        for word in filtered:
+            tweet_words_list.append(word[0])
+
+    counts = Counter(tweet_words_list)
+    return counts
+
+
+dist = get_most_used_words(fetched_tweets, 'NN')
+
+
+def get_words_by_sentiment(tweets, sentiment):
+    tweet_words_list = []
+    for tweet in tweets:
+        tokens = nltk.word_tokenize(clean_tweet(tweet.text))
+        text = nltk.Text(tokens)
+        for token in text:
+            tweet_words_list.append(token)
+    sid = SentimentIntensityAnalyzer()
+    pos_word_list = []
+    neu_word_list = []
+    neg_word_list = []
+
+    for word in tweet_words_list:
+        if (sid.polarity_scores(word)['compound']) >= 0.5:
+            pos_word_list.append(word)
+        elif (sid.polarity_scores(word)['compound']) <= -0.5:
+            neg_word_list.append(word)
+        else:
+            neu_word_list.append(word)
+    if sentiment == 'pos':
+        return pos_word_list
+    elif sentiment == 'neg':
+        return neg_word_list
+    elif sentiment == 'neu':
+        return neu_word_list
+    else:
+        print('invalid sentiment')
+
+
+neg_words = get_words_by_sentiment(fetched_tweets, 'pos')
+
+# r = contain_related_word('we have a freeze')
+# print(r)
+# arr = np.array(tweet_list)
+tweets = pd.read_csv('test.csv')
+print(tweets['content'])
+word_filter = tweets['content'].apply(contain_related_word)
+print(word_filter)
+print(tweets[word_filter])
+# df = pd.DataFrame(arr, columns=['content'])
+#
+# df.to_csv('sample.csv')
+
 
 # Read csv
 
