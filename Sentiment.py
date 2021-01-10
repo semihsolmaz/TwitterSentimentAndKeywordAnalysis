6c848346d7778ecf11c5dc3704e8c6d95aad6eb8
gulsah <keskingulsah92@gmail.com>
2021-01-10 14:42:26 +0300

    exportCSV function initial work

--- Sentiment.py
+++ Sentiment.py
@@ -1,8 +1,25 @@
+import tweepy
+import csv
 from textblob import TextBlob
 
+import nltk
+from nltk.corpus import stopwords
+
+tweetList = []
+
+
 def setSentiment(tweetList):
+    analysis = TextBlob(tweetList).sentiment
+    print(analysis)
 
     return tweetList
 
+
 def exportCSV(tweetList):
-# write list to a csv file
+    csvFile = open(tweetList.csv, 'a')
+
+    csvWriter = csv.writer(csvFile)
+
+    for tweet in tweetList:
+        csvWriter.writerow(["id", "content", "tweetDate"])
+        csvWriter.writerows(tweetList)
