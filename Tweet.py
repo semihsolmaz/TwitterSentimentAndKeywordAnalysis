b522367b4f706a3e8e8c55ad5fc9d6210a51ac8f
semih <semihsolmaz@hotmail.com>
2021-01-12 19:35:51 +0300

    most used words graph

--- Tweet.py
+++ Tweet.py
@@ -4,10 +4,7 @@
 
 import tweepy
 
-auth = tweepy.OAuthHandler('dT8QrbTNl2Iv5RZ2vqqsSB4Ra', 'tpUB3N5MkrQ7tUwYSWpyQI90ytuz8WHyJVoCbZ5o2FlpUFRtEk')
-auth.set_access_token('2483903256-UHDqrWSYTP28TOc5kzQNL3RH3xXJHTiUG3Y4sg1', 'tPSEJnQOwBwr4H1wK2UPZRqMWUAHANqaUrJLKYNjbemjI')
 
-api = tweepy.API(auth)
 
 fetched_tweets = api.search(q='Qatar airways', count=10)
 
